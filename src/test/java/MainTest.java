d7e998001031507fafed4c3832df1610e3916dd2
sah2020 <akmaljonsamandarov@gmail.com>
2022-08-05 10:45:02 +0500

    setup created

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void alwaysTrueTest() {
+        assertTrue(true);
+    }
+}
